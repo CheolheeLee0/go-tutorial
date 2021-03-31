862191ea3e39b04f7586578022ad45909b521142
CheolheeLee0 <wks0968@gmail.com>
2021-03-31 16:08:23 +0900

    http information을 보여주는 gindump 추가

--- server.go
+++ server.go
@@ -5,13 +5,15 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
+	gindump "github.com/tpkeeper/gin-dump" // http information
 
 	"example.com/hello/controller"
 	"example.com/hello/middlewares"
 	"example.com/hello/service"
 )
+
 var (
-	videoService service.VideoService = service.New()
+	videoService    service.VideoService       = service.New()
 	videoController controller.VideoController = controller.New(videoService)
 )
 
@@ -22,10 +24,9 @@ func setupLogOutput() {
 
 func main() {
 	setupLogOutput()
-
 	server := gin.New()
 	// 001 - Logger customizing
-	server.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth())
+	server.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth(), gindump.Dump())
 
 	server.GET("/videos", func(ctx *gin.Context) {
 		ctx.JSON(200, videoController.FindAll())
